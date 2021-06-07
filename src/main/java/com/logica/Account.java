6a2cd9ee3ad185f5f4027d054cd17d9146921891
Luisa Ibagon <jeduard@unicauca.edu.co>
2021-06-07 10:53:36 -0500

    Estructura basica del codigo, sobre del taller sobre Principios (Buenas Practicas) aplicando Object Calisthenics

---
+++ src/main/java/com/logica/Account.java
@@ -0,0 +1,20 @@
+package com.logica;
+
+
+public class Account {
+
+    public void deposito(int monto, String fecha) {
+
+    }
+
+    public void retiro(int monto){
+
+    }
+
+
+    public void extractoBancario() {
+
+    }
+
+
+}
---
+++ src/main/java/com/start/StartApp.java
@@ -0,0 +1,9 @@
+package com.start;
+
+public class StartApp {
+
+    public static void main(String[] args) {
+
+    }
+
+}
