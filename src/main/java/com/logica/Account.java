37a36d254fab3566568f895ac618b7ff28d018c7
Luisa Ibagon <jeduard@unicauca.edu.co>
2021-06-07 14:05:22 -0500

    Funcionalidad para realizar depositos en la cuenta bancaria

--- src/main/java/com/logica/Account.java
+++ src/main/java/com/logica/Account.java
@@ -1,20 +1,75 @@
 package com.logica;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 
 public class Account {
 
-    public void deposito(int monto, String fecha) {
+    private long saldo = 0;
+    private LocalDate fecha;
 
+    ArrayList<Account> mov = new ArrayList<>();
+
+    public long getSaldo() {
+        return saldo;
     }
 
-    public void retiro(int monto){
-
+    public void setSaldo(long saldo) {
+        this.saldo = saldo;
     }
 
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Account() {
+    }
+
+
+    public void deposito(long monto, String fechaDeposito) {
+        Account c = new Account();
+        c.setSaldo(monto);
+        c.setFecha(formatoFecha(fechaDeposito));
+        mov.add(c);
+    }
+
+    public void retiro(int montoRetiro, String fechaRetiro){
+        if (saldoTotal(montoRetiro) == true)
+            deposito(-(montoRetiro), fechaRetiro);
+    }
 
     public void extractoBancario() {
-
+        System.out.println("   Fecha        Movimiento       Balance");
+        for(int i = 0; i< mov.size(); i++) {
+            this.saldo += mov.get(i).getSaldo();
+            System.out.println(mov.get(i).getFecha() + "        " + mov.get(i).getSaldo());
+        }
     }
 
 
+    public LocalDate formatoFecha(String fecha){
+
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate fechaLocal = LocalDate.parse(fecha,df);
+        return fechaLocal;
+    }
+
+    private long calcularSaldo(){
+
+        for(int i = 0; i< mov.size(); i++)
+            this.saldo += mov.get(i).getSaldo();
+        return this.saldo;
+    }
+
+    private boolean saldoTotal(long monto){
+        long balance = this.saldo - monto;
+        if(balance > 0)
+            return true;
+        return false;
+    }
 }
--- src/main/java/com/start/StartApp.java
+++ src/main/java/com/start/StartApp.java
@@ -1,9 +1,19 @@
 package com.start;
 
+import com.logica.Account;
+
+import java.text.ParseException;
+import java.util.Date;
+
 public class StartApp {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws ParseException {
+        Account cuenta = new Account();
+        cuenta.deposito(10000, "01/10/2021");
+        cuenta.deposito(20000, "01/10/2021");
+        cuenta.deposito(30000, "01/10/2021");
+        cuenta.retiro(50000, "12/12/2021");
+        cuenta.extractoBancario();
     }
 
 }
